906292b0b46758816ff9b28903c2d27038b0dbda
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-02-24 10:34:32 +0200

    style

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,12 +3,13 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
+
 function displayImg (images) {
   const galleryContainer = document.querySelector(".image-container");
   galleryContainer.innerHTML = "";
 
   images.forEach((image) => {
-    const imageCard = document.createElement('div');
+    const imageCard = document.createElement('li');
     imageCard.classList.add('image-card');
 
     const imageUrl = image.webformatURL;
@@ -22,17 +23,58 @@ const imageDownloads = image.downloads;
       <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${imageAlt}">
         <img src="${imageUrl}" alt="${imageAlt}" loading="lazy">
       </a>
-      <div class="card-text">
+      <ul class="card-list">
+      <li class="card-list-item">
       <h3 class="card-title">Likes:</h3>
      <p class="card-text"> ${imageLike}</p>
+     </li>
+     <li class="card-list-item">
      <h3 class="card-title">Views:</h3>
      <p class="card-text">${imageView}</p>
+     </li>
+     <li class="card-list-item">
      <h3 class="card-title">Comments:</h3>
      <p class="card-text">${imageComments}</p>
+     </li>
+     <li class="card-list-item">
      <h3 class="card-title">Downloads:</h3>
      <p class="card-text">${imageDownloads}</p>
-     </div>
+     </li>
+     </ul>
     `;
+
+
+
+// function displayImg (images) {
+//   const galleryContainer = document.querySelector(".image-container");
+//   galleryContainer.innerHTML = "";
+
+//   images.forEach((image) => {
+//     const imageCard = document.createElement('div');
+//     imageCard.classList.add('image-card');
+
+//     const imageUrl = image.webformatURL;
+//     const imageAlt = image.tags;
+// const imageLike = image.likes;
+// const imageView = image.views;
+// const imageComments = image.comments;
+// const imageDownloads = image.downloads;
+
+//     imageCard.innerHTML = `
+//       <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${imageAlt}">
+//         <img src="${imageUrl}" alt="${imageAlt}" loading="lazy">
+//       </a>
+//       <div class="card-text">
+//       <h3 class="card-title">Likes:</h3>
+//      <p class="card-text"> ${imageLike}</p>
+//      <h3 class="card-title">Views:</h3>
+//      <p class="card-text">${imageView}</p>
+//      <h3 class="card-title">Comments:</h3>
+//      <p class="card-text">${imageComments}</p>
+//      <h3 class="card-title">Downloads:</h3>
+//      <p class="card-text">${imageDownloads}</p>
+//      </div>
+//     `;
 
     galleryContainer.appendChild(imageCard);
   });
--- src/main.js
+++ src/main.js
@@ -19,22 +19,33 @@ function handleFormSubmit(event) {
     return;
   }
 
-  // loader.style.display = "block";
+  clearGallery();
+
+  loader.style.display = "block";
+
 
   getImages(QUERY)
     .then(data => {
-      // loader.style.display = "none";
+      setTimeout(() => {
+        loader.style.display = "none";
 
-      if (data.hits.length === 0) {
-        iziToast.warning({
-          title: "No Results",
-          message: "Sorry, there are no images matching your search query. Please try again!",
-        });
-      } else {
-        displayImg(data.hits);
-      }
+        if (data.hits.length === 0) {
+          iziToast.warning({
+            message: "Sorry, there are no images matching your search query. Please try again!",
+          });
+        } else {
+          displayImg(data.hits);
+        }
+      }, 2000);
     })
+   
     .catch(error => {
-      // loader.style.display = "none";
+      loader.style.display = "none";
     });
+}
+
+
+function clearGallery() {
+  const galleryContainer = document.querySelector(".image-container");
+  galleryContainer.innerHTML = "";
 }
